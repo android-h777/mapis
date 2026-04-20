406a85d807c652b6501ed9811555544017fc8726
hoo <android.h777@gmail.com>
2026-04-20 15:28:54 +0900

    버튼 개선

--- js/glass_engine.js
+++ js/glass_engine.js
@@ -12,8 +12,19 @@ function glassTrack(e) {
   this.style.setProperty('--ol-angle', angle + 'deg');
 }
 
-/* li.on 트래킹 */
-$(document).on('mousemove', '.mainList li.on', glassTrack);
+/* li.on 트래킹 + 틸트 */
+$(document).on('mousemove', '.mainList li.on', function(e) {
+  glassTrack.call(this, e);
+  var rect = this.getBoundingClientRect();
+  var x = (e.clientX - rect.left) / rect.width;
+  var y = (e.clientY - rect.top) / rect.height;
+  var tiltX = (0.5 - y) * 4;
+  var tiltY = (x - 0.5) * 3;
+  this.style.transform = 'translateY(-2px) perspective(800px) rotateX(' + tiltX + 'deg) rotateY(' + tiltY + 'deg)';
+});
+$(document).on('mouseleave', '.mainList li.on', function() {
+  this.style.transform = 'translateY(-2px)';
+});
 
 /* hBtn 트래킹 */
 $(document).on('mousemove', '.hBtn', glassTrack);
